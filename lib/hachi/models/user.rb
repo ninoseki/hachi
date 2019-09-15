8eab97bcf17a82add351af8ca8a6e6c935176823
Manabu Niseki <manabu.niseki@gmail.com>
2019-09-15 12:14:36 +0900

    feat: add user API support

---
+++ lib/hachi/models/user.rb
@@ -0,0 +1,39 @@
+# frozen_string_literal: true
+
+module Hachi
+  module Models
+    class User < Base
+      attr_reader :login
+      attr_reader :name
+      attr_reader :roles
+      attr_reader :password
+
+      ROLES = %w(read write admin).freeze
+
+      def initialize(login:, name:, roles:, password:)
+        @login = login
+        @name = name
+        @roles = roles
+        @password = password
+
+        validate_roles
+      end
+
+      def payload
+        {
+          login: login,
+          name: name,
+          roles: roles,
+          password: password
+        }.compact
+      end
+
+      private
+
+      def validate_roles
+        raise ArgumentError, "roles should be an array" unless roles.is_a?(Array)
+        raise ArgumentError, "role should be one of #{ROLES.join('.')}" unless roles.all? { |role| ROLES.include? role }
+      end
+    end
+  end
+end
