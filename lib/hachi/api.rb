f50b25f340e7943cccc54085d2a7c31af5b3c70e
Manabu Niseki <manabu.niseki@gmail.com>
2019-10-06 20:05:23 +0900

    refactor: change instances to methods

--- lib/hachi/api.rb
+++ lib/hachi/api.rb
@@ -2,19 +2,31 @@
 
 module Hachi
   class API
-    attr_reader :alert
-    attr_reader :artifact
-    attr_reader :case
-    attr_reader :user
+    attr_reader :api_endpoint
+    attr_reader :api_key
 
     def initialize(api_endpoint: ENV["THEHIVE_API_ENDPOINT"], api_key: ENV["THEHIVE_API_KEY"])
-      raise(ArgumentError, "api_endpoint argument is required") unless api_endpoint
-      raise(ArgumentError, "api_key argument is required") unless api_key
+      @api_endpoint = api_endpoint
+      raise ArgumentError, "api_endpoint argument is required" unless api_endpoint
 
-      @alert = Clients::Alert.new(api_endpoint: api_endpoint, api_key: api_key)
-      @artifact = Clients::Artifact.new(api_endpoint: api_endpoint, api_key: api_key)
-      @case = Clients::Case.new(api_endpoint: api_endpoint, api_key: api_key)
-      @user = Clients::User.new(api_endpoint: api_endpoint, api_key: api_key)
+      @api_key = api_key
+      raise ArgumentError, "api_key argument is required" unless api_key
+    end
+
+    def alert
+      @alert ||= Clients::Alert.new(api_endpoint: api_endpoint, api_key: api_key)
+    end
+
+    def artifact
+      @artifact ||= Clients::Artifact.new(api_endpoint: api_endpoint, api_key: api_key)
+    end
+
+    def case
+      @case ||= Clients::Case.new(api_endpoint: api_endpoint, api_key: api_key)
+    end
+
+    def user
+      @user ||= Clients::User.new(api_endpoint: api_endpoint, api_key: api_key)
     end
   end
 end
