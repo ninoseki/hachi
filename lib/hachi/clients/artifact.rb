fc9438debb13e5eb9f5bfad9f7dc1f92f1248994
Manabu Niseki <manabu.niseki@gmail.com>
2019-11-30 14:32:19 +0900

    docs: add YARD annotations to main methods

--- lib/hachi/clients/artifact.rb
+++ lib/hachi/clients/artifact.rb
@@ -3,6 +3,18 @@
 module Hachi
   module Clients
     class Artifact < Base
+      #
+      # Create an artifact
+      #
+      # @param [String] case_id Artifact ID
+      # @param [String] data
+      # @param [String] data_type
+      # @param [String, nil] message
+      # @param [Integer, nil] tlp
+      # @param [Array<String>, nil] tags
+      #
+      # @return [Hash]
+      #
       def create(case_id, data:, data_type:, message: nil, tlp: nil, tags: nil)
         artifact = Models::Artifact.new(
           data: data,
@@ -15,18 +27,47 @@ module Hachi
         post("/api/case/#{case_id}/artifact", artifact.payload) { |json| json }
       end
 
+      #
+      # Get an artifact
+      #
+      # @param [String] id Artifact ID
+      #
+      # @return [Hash]
+      #
       def get_by_id(id)
         get("/api/case/artifact/#{id}") { |json| json }
       end
 
+      #
+      # Delete an artifact
+      #
+      # @param [String] id Artifact ID
+      #
+      # @return [String]
+      #
       def delete_by_id(id)
         delete("/api/case/artifact/#{id}") { |json| json }
       end
 
+      #
+      # Find artifacts
+      #
+      # @param [Hash] attributes
+      # @param [String] range
+      #
+      # @return [Array]
+      #
       def search(attributes, range: "all")
         _search("/api/case/artifact/_search", attributes: attributes, range: range) { |json| json }
       end
 
+      #
+      # Get list of similar observables
+      #
+      # @param [String] id Artifact ID
+      #
+      # @return [Array]
+      #
       def similar(id)
         get("/api/case/artifact/#{id}/similar") { |json| json }
       end
